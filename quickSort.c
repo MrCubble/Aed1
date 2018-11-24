07695e10e7d97bdf56b1941cf5a94c7263d780ac
Lucas Rocha <lucas-2470@hotmail.com>
2018-11-23 20:05:38 -0400

    Update quickSort.c

--- quickSort.c
+++ quickSort.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
+#include <string.h>
+typedef struct tipoDado{
+    char nome[30];
+}tipoDado;
 
-void insectionSort(int v[], int ini, int fim){
-    int *i, *j;
-    int *pfim = &v[fim];
-    int *pini = &v[ini];
-    int pivot;
+void insectionSort(tipoDado v[], int ini, int fim){
+    tipoDado *i, *j;
+    tipoDado *pfim = &v[fim];
+    tipoDado *pini = &v[ini];
+    tipoDado pivot;
     for(j = &v[ini+1]; j<=pfim; j++){
         i = j -1;
         pivot = *j;
-        while((i>=pini) && (*i>pivot)){
+        while((i>=pini) && (strcmp(i->nome, pivot.nome)>0)){
             *(i + 1) = *i;
             i--;
         }
@@ -17,23 +21,47 @@ void insectionSort(int v[], int ini, int fim){
 }
 
 void qSortInterno(int v[], int ini, int fim){
+    int i, j;
+    int pivot, tmp;
+    if(fim - ini > 0){
+        i = ini;
+        j = fim;
+        pivot = v[(i+j)/2];
+        do{
+            while(v[i] < pivot) i++;
+            while(v[j] > pivot) j--;
+            if(i<=j){
+                tmp = v[i];
+                v[i] = v[j];
+                v[j] = tmp;
+                i++; j--;
+            }
+        }while(i<=j);
+        qSortInterno(v, ini, j);
+        qSortInterno(v, i, fim);
+    }
+}
+
+void qSortInterno2(tipoDado v[], int ini, int fim){
     if(fim - ini <= 10)
         insectionSort(v, ini, fim);
     else{
-        qSortInterno(v, ini, (ini+fim)/2);
-        qSortInterno(v, (ini+fim/2), fim);
+        qSortInterno2(v, ini, (ini+fim)/2);
+        qSortInterno2(v, (ini+fim/2), fim);
     }
 }
-void quickSort(int n, int v[]){
-    qSortInterno(v, 0, n-1);
+void quickSort(int n, tipoDado v[]){
+    qSortInterno2(v, 0, n-1);
 }
 
 int main(){
-    int n, v[1001];
+    int n;
+    tipoDado v[1001];
     scanf("%d", &n);
     for(int i=0; i<n; i++)
-        scanf("%d", &v[i]);
+        scanf("%s", v[i].nome);
     quickSort(n, v);
     for(int i=0; i<n; i++)
-        printf("%d ", v[i]);
+        printf("%s ", v[i].nome);
+    printf("\n");
 }
